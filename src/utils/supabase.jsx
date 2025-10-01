9a4090063e3afbe30570f6efb0b588eefa1da178
Dinuvi9170 <dinuvitharushika@gmail.com>
2025-10-01 23:51:56 +0530

    remove tracking supabase✏️

--- src/utils/supabase.jsx
+++ src/utils/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url="https://mxlpkdrudnaffxfgykil.supabase.co"
-const key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bHBrZHJ1ZG5hZmZ4Zmd5a2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMjkwMDIsImV4cCI6MjA3NDgwNTAwMn0.AoHq7DNO4KZzfg1hEIV-iWWm9C6Cb5AAClflcc8TdjI"
+const url=import.meta.env.VITE_SupabaseUrl
+const key=import.meta.env.VITE_SupabaseKey
 const supabase = createClient(url,key);
 export default supabase;
